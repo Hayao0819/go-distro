79c498a0e33e0aed48ee399334234dfbca1e02a7
hayao <hayao@fascode.net>
2023-04-24 01:29:01 +0900

    Move: goos module

---
+++ goos/goos.go
@@ -0,0 +1,43 @@
+package goos
+
+import (
+	"runtime"
+)
+
+type G int
+const (
+	Aix G = iota
+	Android
+	Darwin
+	Dragonfly
+	Freebsd
+	Illumos
+	Ios
+	Js
+	Linux
+	Netbsd
+	Openbsd
+	Plan9
+	Solaris
+	Windows
+)
+
+func Get()(G){
+	switch runtime.GOOS {
+		case "AIX": return Aix
+		case "android": return Android
+		case "darwin": return Darwin
+		case "dragonfly": return Dragonfly
+		case "freebsd": return Freebsd
+		case "illumos": return Illumos
+		case "ios": return Ios
+		case "js": return Js
+		case "linux": return Linux
+		case "netbsd": return Netbsd
+		case "openbsd": return Openbsd
+		case "plan9": return Plan9
+		case "solaris": return Solaris
+		case "windows": return Windows
+		default: return -1
+	}
+}
