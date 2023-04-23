b7a9f2a0252dc602a33b1efc963fbc2b44110dd4
hayao <hayao@fascode.net>
2023-04-24 01:28:24 +0900

    Update: Add DistroList and  Other

--- linux/distrobution.go
+++ linux/distrobution.go
@@ -1,6 +1,29 @@
 package linux
 
-var Other  = &Linux{value: "other"}
+import "github.com/Hayao0819/go-distro/ostype"
+
+var DistroList []*Linux = []*Linux{
+	Other,
+	Arch,
+	Debian,
+	Ubuntu,
+	RHEL,
+	CentOS,
+}
+
+var Other  = &Linux{
+	value: "other",
+	verfunc: func()(ostype.V){
+		return Version{
+			id: "none",
+			codename: "none",
+		}
+	},
+	require: func()(bool){
+		return true
+	},
+}
+
 var	Arch   = &Linux{value: "arch"}
 var	Debian = &Linux{value: "debian"}
 var	Ubuntu = &Linux{value: "ubuntu"}
