0dedb7bf611336fedee70403e1147a3303a0c267
hayao <hayao@fascode.net>
2023-04-24 01:27:44 +0900

    Add: other os

--- ostype/common.go
+++ ostype/common.go
@@ -2,9 +2,24 @@ package ostype
 
 type F interface {
 	String() string
+	//Version() V
 }
 
+// バージョン
 type V interface{
 	ID() string
 	CodeName() string
 }
+
+type other struct{}
+func (o other)String()(string){
+	return "other"
+}
+func (o other)ID()(string){
+	return "other"
+}
+func (o other)CodeName()(string){
+	return "other"
+}
+
+var Other = other{}
