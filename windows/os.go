60251bff3ffefa22fbd6563ab19b7add029136a2
hayao <hayao@fascode.net>
2023-04-25 00:19:16 +0900

    Add: windows

---
+++ windows/os.go
@@ -0,0 +1,66 @@
+package windows
+
+import (
+	"github.com/Hayao0819/go-distro/ostype"
+	//"golang.org/x/sys/windows/registry"
+)
+
+
+type Version struct {
+	codename string
+	id  string
+}
+
+type Win struct {
+	version ostype.V
+}
+
+func (w Win)ID() string{
+	return "windows"
+}
+
+func (w Win)Name() string{
+	return "Windows"
+}
+func (w Win)Version() ostype.V{
+	return w.version
+}
+
+func (v Version)ID() string{
+	return v.id
+}
+func (v Version)CodeName() string{
+	return v.codename
+}
+
+var (
+	Other = &Win{
+		version: Version{
+			id:       "other",
+			codename: "other",
+		},
+	}
+	Win7 = &Win{
+		version: Version{
+			id:       "7",
+			codename: "Vienna",
+		},
+	}
+	Win10 = &Win{
+		version: Version{
+			id:       "10",
+			codename: "Redstone",
+		},
+	}
+	Win11 = &Win{
+		version: Version{
+			id:       "11",
+			codename: "Sun Valley",
+		},
+	}
+)
+
+// Todo: Get Windows Version
+func Get()(*Win){
+	return Other
+}
